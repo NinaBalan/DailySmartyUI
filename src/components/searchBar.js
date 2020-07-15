bf19e4541555c3043c06c012f69b83698194a516
NinaBalan <52449291+NinaBalan@users.noreply.github.com>
2020-07-15 12:38:34 +0300

    setup app routes

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -5,6 +5,7 @@ import { Field, reduxForm } from 'redux-form';
 class SearchBar extends Component {
     handleFormSubmit = function({query}) {
         console.log('trying to handle subimit for query', query);
+        //navigate to a new route.
     }
 
     renderInput(field) {
